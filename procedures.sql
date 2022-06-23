987ac6fef424613020bf464a4471e2e2758ea11f
moisessosa <77083327+moisessosa@users.noreply.github.com>
2022-06-23 15:48:48 -0300

    2 versiones
    la segunda version tiene un parametro tipo OUT

--- procedures.sql
+++ procedures.sql
@@ -18,3 +18,31 @@ EXCEPTION
 end VISUALIZAR;
 /
 EXECUTE VISUALIZAR(110);
+-- version 2
+-- tieme parameto tipo out para el total de empleados
+create or replace procedure VISUALIZAR(DEPT EMPLOYEES.DEPARTMENT_ID%TYPE, t out number) is
+
+cursor empleado is select first_name, last_name, salary from EMPLOYEES WHERE DEPARTMENT_ID = DEPT; 
+begin
+t:=0;
+for i in empleado loop
+    t := t+1;
+    dbms_output.put_line(i.first_name || ' '|| i.last_name || ' ' ||i.salary);
+end loop;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        
+        dbms_output.put_line('NO EXISTE EL DEPARTAMENTO');
+    WHEN OTHERS THEN
+        dbms_output.put_line('ERROR ');
+
+end VISUALIZAR;
+/
+declare
+total number;
+depart number:=100;
+begin
+VISUALIZAR(depart, total);
+dbms_output.put_line('Total de empleados en el departamento '||depart|| ' es de: '|| total); 
+end;
+/
